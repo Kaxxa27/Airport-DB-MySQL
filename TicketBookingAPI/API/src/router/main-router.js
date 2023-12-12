351cf7089210b51b1c28d0827cc5c50ad32fc4f0
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-12 22:52:33 +0300

    Added CRUD for nationality.

--- TicketBookingAPI/API/src/router/main-router.js
+++ TicketBookingAPI/API/src/router/main-router.js
@@ -4,5 +4,6 @@ router.use('/auth', require('./auth-router'));
 router.use('/user', require('./user-router'));
 router.use('/passport', require('./passport-router'));
 router.use('/gender', require('./gender-router'));
+router.use('/nationality', require('./nationality-router'));
 
 module.exports = router;
