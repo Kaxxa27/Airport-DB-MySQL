302f3ee4af1ddcfb190f2f5797e390276b831f71
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-12 22:56:25 +0300

    Added CRUF for Country.

--- TicketBookingAPI/API/src/router/main-router.js
+++ TicketBookingAPI/API/src/router/main-router.js
@@ -5,5 +5,6 @@ router.use('/user', require('./user-router'));
 router.use('/passport', require('./passport-router'));
 router.use('/gender', require('./gender-router'));
 router.use('/nationality', require('./nationality-router'));
+router.use('/country', require('./country-router'));
 
 module.exports = router;
