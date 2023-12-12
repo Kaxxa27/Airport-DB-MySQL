ff0412c69604a8a2245f5eb8a5067c78ed31f964
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-12 22:47:55 +0300

    Added CRUD for gender.

--- TicketBookingAPI/API/src/router/main-router.js
+++ TicketBookingAPI/API/src/router/main-router.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/auth', require('./auth-router'));
 router.use('/user', require('./user-router'));
 router.use('/passport', require('./passport-router'));
+router.use('/gender', require('./gender-router'));
 
 module.exports = router;
