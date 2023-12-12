6b4e9c5a0e027ba683a37bf43083d9b77d3d03b1
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-12 22:38:50 +0300

    Added CRUD for passport.

--- TicketBookingAPI/API/src/router/main-router.js
+++ TicketBookingAPI/API/src/router/main-router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/auth', require('./auth-router'));
 router.use('/user', require('./user-router'));
+router.use('/passport', require('./passport-router'));
 
 module.exports = router;
