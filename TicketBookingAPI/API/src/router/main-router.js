79b3481aa97bb0c3a4afea2441e9d6b65c52f6c6
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-12-12 23:07:24 +0300

    Added CRUD for Event.

--- TicketBookingAPI/API/src/router/main-router.js
+++ TicketBookingAPI/API/src/router/main-router.js
@@ -7,5 +7,6 @@ router.use('/gender', require('./gender-router'));
 router.use('/nationality', require('./nationality-router'));
 router.use('/country', require('./country-router'));
 router.use('/city', require('./city-router'));
+router.use('/event', require('./event-router'));
 
 module.exports = router;
